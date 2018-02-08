071f3d44a993a72a6db578e76e9b8a7f3e68a542
Syed Muhammad Sohaib <33151569+syedMSohaib@users.noreply.github.com>
2018-02-08 23:50:18 +0500

    Inheritance & Interface in Solidity

---
+++ Inheritance.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.11;
+
+interface Regulator{
+    function checkValue(uint amount) returns (bool);
+    function loan() returns (bool);
+}
+
+contract Bank is Regulator {
+    uint private value;
+    
+    function Bank(uint amount){
+        value = amount;
+    }
+    
+    function deposit(uint amount){
+        value += amount;
+    }
+    
+    function widthdraw(uint amount){
+        if ( checkValue(amount) ){
+            value -= amount;
+        }
+    }
+    
+    function balance() returns (uint) {
+        return value;
+    }
+    
+    function checkValue(uint amount) returns (bool) {
+        return value >- amount;
+    }
+    
+    function loan() returns (bool){
+        return value > 0;
+    }
+    
+}
+
+contract MyFirstContract is Bank(10) {
+    string private name;
+    uint private age;
+    
+    function setName(string newName) public{
+        name = newName;
+    }
+    
+    function getName() returns (string)
+    {
+         return name;
+    }
+    
+    function setAge(uint newAge){
+        age = newAge;
+    }
+    
+    function getAge() returns (uint){
+        return age;
+    }
+    
+}
